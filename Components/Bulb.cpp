e6d06905710f94541ed0b83170abb7e65de798ff
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2021-12-31 15:03:34 +0200

    updated bulb 1.1

--- Components/Bulb.cpp
+++ Components/Bulb.cpp
@@ -1,11 +1,9 @@
 #include "Bulb.h"
 
-//ApplicationManager* pManager;
-//UI* pUI = pManager->GetUI();
-//Bulb* pBulb = nullptr;
-
-Bulb::Bulb(GraphicsInfo* r_GfxInfo) :Component(r_GfxInfo)
-{}
+Bulb::Bulb(GraphicsInfo* r_GfxInfo, double resistance) :Component(r_GfxInfo)
+{
+	this->Internal_res = resistance;
+}
 
 void Bulb::Draw(UI* pUI)
 {
@@ -16,40 +14,3 @@ void Bulb::Draw(UI* pUI)
 
 void Bulb::Operate() {}
 
-Bulb::Bulb()		// no arg constructor
-{
-	Internal_res = 0;
-	isTurn = false;
-	bulb_num += 1;
-}
-Bulb::Bulb(double Internal_res, bool isTurn)		// Arg constructor
-{
-	double Internal_resistance = Internal_res;
-	bool isTurnON = isTurn;
-	bulb_num += 1;
-}
-
-// functions
-void Bulb::setInter_Resistance()
-{
-	//pUI->PrintMsg("Enter the Internal Resistance: \n");
-	//pUI-> GetSrting(Internal_res);
-	//pUI->ClearStatusBar();
-}
-double Bulb::getInter_Resistance()
-{
-	return Internal_res;
-	//pUI->ClearStatusBar();
-}
-
-bool Bulb::Display_Light()
-{
-	//pUI->PrintMsg("Turn on (y or n ) ? ");
-	string option;
-	//option = pUI->GetSrting();
-	if (option == "y" || option == "Y")
-		isTurn = true;
-	else
-		isTurn = false;
-	return isTurn;
-}
