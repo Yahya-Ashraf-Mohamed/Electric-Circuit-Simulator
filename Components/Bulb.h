315b56ba228e6d1ae3c0f7c9ab3380d0c3f728aa
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2021-12-31 15:04:33 +0200

    updated bulb 1.1

--- Components/Bulb.h
+++ Components/Bulb.h
@@ -3,25 +3,20 @@
 #include "..//ApplicationManager.h"
 
 
-
 class Bulb : public Component
 {
 private:
 	double Internal_res;
-	bool isTurn;
-	int bulb_num = 0;
+	/*bool isTurn;
+	int bulb_num = 0;*/
 public:
-	// constructors
-	Bulb();
-	Bulb(GraphicsInfo* r_GfxInfo);
-	Bulb(double in_res, bool state);
+	
+	Bulb(GraphicsInfo* r_GfxInfo, double resistance);
+
 
 	// functions
 	virtual void Operate();	//Calculates the volt on both terminals
 	virtual void Draw(UI*);	//Draws the resistor
-	void setInter_Resistance();
-	double getInter_Resistance();
-
-	bool Display_Light();
+	
 
 };
