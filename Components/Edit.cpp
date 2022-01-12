77754df03f43bb9e33250e90b52caf86ce962936
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2022-01-12 06:19:52 +0200

    Update Edit.cpp

--- Components/Edit.cpp
+++ Components/Edit.cpp
@@ -1,46 +1,24 @@
 #include "Edit.h"
-#include <iostream>
-#include<string.h>
-using namespace std;
 
 
 
-	int Edit::choose_option()
+ApplicationManager* pManager = nullptr;
+UI* pUI = pManager->GetUI();
+
+Edit::Edit(ApplicationManager* pApp) : Component() 
+{
+	pUI = pManager->GetUI();
+}
+
+void Edit::Execute()
+{
+
+	if (pManager->is_one_Comp_selected() == false)
 	{
-		cout << "Do you want to chnge the name of 1_Component OR 2_Connection \nEnter number :" << endl;
-		int choice;
-		cin >> choice;
-		return choice;
+		pUI->PrintMsg("One component must be selected Only!");
+		return;
 	}
-	void Edit::the_comp_conn_changed(int choice)
-	{
-		if (choice == 1)
-		{
-			cout << "Enter the name of the component: " << endl;
-			cin >> old_name_of_comp;
-		}
-		else
-		{
-			cout << "Enter the name of the connection: " << endl;
-			cin >> old_name_of_conn;
-		}
-	}
+	Component* pcomp = nullptr;
+	
 
-	void Edit::the_edited_comp()
-	{
-		cout << "Enter the new name of component : \n ";
-		cin >> new_name_of_comp;
-
-		old_name_of_comp = new_name_of_comp;
-
-		cout << "the new name is : ";
-		cout << old_name_of_comp;
-
-	}
-	void Edit::the_edited_conn()
-	{
-		cout << "Enter the new name of connection: \n";
-		cin >> new_name_of_conn;
-
-		old_name_of_conn = new_name_of_conn;
-	}
+}
