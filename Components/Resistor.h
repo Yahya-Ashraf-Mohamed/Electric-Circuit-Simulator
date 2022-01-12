829fbbfdebe5243e2e664f375c70c25032c90381
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2022-01-12 06:22:48 +0200

    update

--- Components/Resistor.h
+++ Components/Resistor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "..//ApplicationManager.h"
 #include "Component.h"
 #include <string>
 #include <iostream>
@@ -13,24 +14,27 @@ public:
 	virtual void Operate();	//Calculates the volt on both terminals
 	virtual void Draw(UI*);	//Draws the resistor
 
+
 	Resistor();		// No_Arg Constructor
 
-	Resistor(string name, int number, double Value);		// Arg_constructor
+	Resistor(string name, double Value);		// Arg_constructor
 
-	void set_name_resistor();
-	string get_name_resistor(string Nam_Of_res);
-	void set_value_resistor();	// limitations of resistance value
+	void set_label(string resistance_name);
+	string get_label();
+
+	void set_value_resistor(double resistance);	
 	double get_value_resistor();
 
 
-
+	void load(string label, double value);
 	virtual ~Resistor();
 
 
 protected:
 	string Nam_Of_res;
-	// Num_Of_res;
+
 	double Val_Of_res;
-	// double Resis[];
+
 };
+
 
