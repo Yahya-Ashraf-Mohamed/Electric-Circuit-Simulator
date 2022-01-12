89ded5656209b09dc72744b955469b6d0b53eb76
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2022-01-12 06:26:26 +0200

    update

--- Actions/ActionAddBuzzer.h
+++ Actions/ActionAddBuzzer.h
@@ -1,26 +1,22 @@
-//#pragma once
-//#include "..//Actions/Action.h"
-//#include "..//Components/Buzzer.h"
-//#include "..//ApplicationManager.h"
-//
-//
-////adding a new Buzzer action
-//class ActionAddBuz : public Action
-//{
-//private:
-//	//Parameters for rectangular area to be occupied by the comp
-//	int Cx, Cy;	//Center point of the comp
-//	int x1, y1, x2, y2;	//Two corners of the rectangluar area
-//public:
-//	ActionAddBuz(ApplicationManager* pApp);
-//	virtual ~ActionAddBuz(void);
-//
-//	//Execute action (code depends on action type)
-//	virtual void Execute();
-//
-//	virtual void Undo();
-//	virtual void Redo();
-//
-//};
-//
-//#pragma once
+#pragma once
+#include "Actions/Action.h"
+#include "Buzzer.h"
+
+//adding a new Buzzer action
+class ActionAddBuz : public Action
+{
+private:
+	//Parameters for rectangular area to be occupied by the comp
+	int Cx, Cy;	//Center point of the comp
+	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+public:
+	ActionAddBuz(ApplicationManager* pApp);
+	virtual ~ActionAddBuz(void);
+
+	//Execute action (code depends on action type)
+	virtual void Execute();
+
+	virtual void Undo();
+	virtual void Redo();
+
+};
