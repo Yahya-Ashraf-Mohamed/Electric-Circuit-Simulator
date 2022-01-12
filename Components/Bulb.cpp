33b801de021c44df76d604da7b3457d448b88f10
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2022-01-12 06:04:47 +0200

    update

--- Components/Bulb.cpp
+++ Components/Bulb.cpp
@@ -1,36 +1,74 @@
-#pragma once
-#include "Component.h"
-#include "../ApplicationManager.h"
-class Bulb : public Component
+#include "Bulb.h"
+
+Bulb::Bulb(GraphicsInfo* r_GfxInfo, double x) :Component(r_GfxInfo)
 {
-private:
-	double Internal_res;
-	bool isTurn;
-	int bulb_num = 0;
-	string bulb_name;
-public:
-	// constructors
-	Bulb();
-	Bulb(GraphicsInfo* r_GfxInfo);
-	Bulb(GraphicsInfo* r_GfxInfo, double internal);
-	//Bulb(double in_res, bool state);
-	ApplicationManager* pManager = nullptr;
-	UI* pUI = pManager->GetUI();
+	this->Internal_res = x;
+}
 
 
-	// functions
-	virtual void Operate();	//Calculates the volt on both terminals
-	virtual void Draw(UI*);	//Draws the resistor
-	void setInter_Resistance();
-	double getInter_Resistance();
+void Bulb::Draw(UI* pUI)
+{
+	//Call output class and pass resistor drawing info to it.
+	pUI->Draw_Bulb(*m_pGfxInfo); //update to draw resistor
 
-	void set_label(string name);
-	string get_label();
+}
 
-	bool Display_Light();
+void Bulb::Operate() {}
 
-	void load(string label, double value);
+Bulb::Bulb()		// no arg constructor
+{
+	Internal_res = 0;
+	isTurn = false;
+	this->bulb_num += 1;
+}
 
-	~Bulb();
+//Bulb::Bulb(double Internal_res, bool isTurn)		// Arg constructor
+//{
+//	this -> Internal_res = Internal_res;
+//	this -> isTurn = isTurn;
+//	this->bulb_num += 1;
+//}
 
-};
+// functions
+void Bulb::setInter_Resistance()
+{
+	pUI->PrintMsg("Enter the Internal Resistance: \n");
+	string internal_res = pUI->GetSrting();
+	Internal_res = stoi(internal_res);
+	pUI->ClearStatusBar();
+}
+double Bulb::getInter_Resistance()
+{
+	return Internal_res;
+	pUI->ClearStatusBar();
+}
+
+bool Bulb::Display_Light()
+{
+	pUI->PrintMsg("Turn on (y or n ) ? ");
+	string option;
+	option = pUI->GetSrting();
+	if (option == "y" || option == "Y")
+		isTurn = true;
+	else
+		isTurn = false;
+	return isTurn;
+}
+
+void Bulb::set_label(string name)
+{
+	bulb_name = name;
+}
+
+string Bulb::get_label()
+{
+	return bulb_name;
+}
+
+void Bulb::load(string label, double value)
+{
+
+	this->Internal_res = value;
+	label = label;
+}
+
