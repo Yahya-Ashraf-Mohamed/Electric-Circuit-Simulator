2410db0d0e38fefc6cd802a1d4630cf6dcd1e748
WessamZaid <95138931+WessamZaid@users.noreply.github.com>
2022-01-12 06:25:43 +0200

    Update ActionAddBuzzer.cpp

--- Actions/ActionAddBuzzer.cpp
+++ Actions/ActionAddBuzzer.cpp
@@ -1,45 +1,46 @@
-//#include "..//Actions/ActionAddBuzzer.h"
-//
-//ActionAddBuz::ActionAddBuz(ApplicationManager* pApp) :Action(pApp)
-//{}
-//
-//ActionAddBuz::~ActionAddBuz(void)
-//{}
-//
-//void ActionAddBuz::Execute()
-//{
-//
-//	//Get a Pointer to the user Interfaces
-//	UI* pUI = pManager->GetUI();
-//
-//	//Print Action Message
-//	pUI->PrintMsg("Adding a new Buzzer: Click anywhere to add");
-//
-//	//Get Center point of the area where the Comp should be drawn
-//	pUI->GetPointClicked(Cx, Cy);
-//
-//	//Clear Status Bar
-//	pUI->ClearStatusBar();
-//
-//
-//
-//	GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
-//
-//	//Calculate the rectangle Corners
-//	int compWidth = pUI->getCompWidth();
-//	int compHeight = pUI->getCompHeight();
-//
-//	pGInfo->PointsList[0].x = Cx - compWidth / 2;
-//	pGInfo->PointsList[0].y = Cy - compHeight / 2;
-//	pGInfo->PointsList[1].x = Cx + compWidth / 2;
-//	pGInfo->PointsList[1].y = Cy + compHeight / 2;
-//
-//	//Buzzer* pB = new Buzzer(pGInfo);
-//	//pManager->AddComponent(pB);
-//}
-//
-//void ActionAddBuz::Undo()
-//{}
-//
-//void ActionAddBuz::Redo()
-//{}
+#include "ActionAddBuz.h"
+#include "ApplicationManager.h"
+
+ActionAddBuz::ActionAddBuz(ApplicationManager* pApp) :Action(pApp)
+{}
+
+ActionAddBuz::~ActionAddBuz(void)
+{}
+
+void ActionAddBuz::Execute()
+{
+
+	//Get a Pointer to the user Interfaces
+	UI* pUI = pManager->GetUI();
+
+	//Print Action Message
+	pUI->PrintMsg("Adding a new Buzzer: Click anywhere to add");
+
+	//Get Center point of the area where the Comp should be drawn
+	pUI->GetPointClicked(Cx, Cy);
+
+	//Clear Status Bar
+	pUI->ClearStatusBar();
+
+
+
+	GraphicsInfo* pGInfo = new GraphicsInfo(2); //Gfx info to be used to construct the Comp
+
+	//Calculate the rectangle Corners
+	int compWidth = pUI->getCompWidth();
+	int compHeight = pUI->getCompHeight();
+
+	pGInfo->PointsList[0].x = Cx - compWidth / 2;
+	pGInfo->PointsList[0].y = Cy - compHeight / 2;
+	pGInfo->PointsList[1].x = Cx + compWidth / 2;
+	pGInfo->PointsList[1].y = Cy + compHeight / 2;
+
+	//Buzzer* pB = new Buzzer(pGInfo);
+	//pManager->AddComponent(pB);
+}
+
+void ActionAddBuz::Undo()
+{}
+
+void ActionAddBuz::Redo()
+{}
